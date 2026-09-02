16de021b015d7b707c5654824f2d1d2cf303e685
Eliad Hagag <eliadhagag3@gmail.com>
2026-09-02 13:55:13 +0300

    style: update CollectorSearch card button label to Upgrade to VIP to Message

--- frontend/src/pages/CollectorSearch.jsx
+++ frontend/src/pages/CollectorSearch.jsx
@@ -75,7 +75,7 @@ function CollectorCard({ collector, index, onMessage }) {
               whileHover={{ y: -1, boxShadow: '2px 4px 0px rgba(0,0,0,0.7)' }}
               whileTap={{ y: 0, boxShadow: '1px 1px 0px rgba(0,0,0,0.7)' }}
             >
-              👑 Upgrade to VIP
+              👑 Upgrade to VIP to Message
             </motion.button>
           ) : !targetIsVipOrAdmin ? (
             <button
